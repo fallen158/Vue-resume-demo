feaf6101a7557b42b89204958a49c2be8c13f94b
fallen158 <79334424@qq.com>
2018-06-24 22:16:23 +0800

    注册登录框框

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         editingName: false,
+        loginVisible: false,
+        singUpVisible: false,
         resume: {
             name: '姓名',
             title: '前端开发工程师',
@@ -14,6 +16,33 @@ var app = new Vue({
     methods: {
         onEdit(key, value) {
             this.resume[key] = value
+        },
+        showLogin(e) {
+            this.loginVisible = true
+        },
+        saveResume() {
+
+        },
+        onSubmit() {
+
+        },
+        onClickSave() {
+            console.log(this.resume)
+                // var User = AV.Object.extend('User');
+                // var user = new User();
+                // user.set('resume', this.resume);
+                // user.save().then(function(user) {
+                //     console.log(user);
+                // }, function(error) {
+                //     console.error(error);
+                // });
+
+            var currentUser = AV.User.current();
+            if (!currentUser) {
+                this.showLogin()
+            } else {
+                this.saveResume()
+            }
         }
     }
 })
